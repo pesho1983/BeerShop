5d5815545dfdb8c43b050ff0525c8f8fb34b3ada
TodorMiroleskov <tmiroleskov@qualityhouse.com>
2018-03-19 17:19:17 +0200

    Homepage fixes

--- QH_Beer/Templates/homepage/index.php
+++ QH_Beer/Templates/homepage/index.php
@@ -13,72 +13,72 @@
     <header>
       <?php include_once "../beer/header.php"; ?>
     </header>  
+
+    <div class="container">   
+
+    <div class="row">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-8">
+        
+            <div class="main_info">
+            <h2 class="welcome">Welcome to Quality House Beer Website</h2>
+            <p>We are Quality House Beer Company and we started our beer production business in 2015.
+            Our mission is to provide the customer with the desired pleasure of consuming quality beer.
+            That's why we started this project with enthusiasm and over the years we have made efforts to improve the taste of our beer.</p>
+            </div>
+            <div class="col-lg-12 beers">           
+                <div class="col-lg-12 best_beers">
+                    <h2 class="welcome">BEST SELLING BEERS</h2>
+                    <div>
+                        <p><img src="../../../Vendor/images/heineken.png" ></p>
+                        <p>Name 1: some info</p>
+                    </div>
+                    <div>
+                        <p><img src="../../../Vendor/images/heineken.png" ></p>
+                        <p>Name 1: some info</p>
+                    </div>
+                    <div>
+                        <p><img src="../../../Vendor/images/heineken.png" ></p>
+                        <p>Name 1: some info</p>
+                    </div>
+                    <div>
+                        <p><img src="../../../Vendor/images/heineken.png"></p>
+                        <p>Name 1: some info</p>
+                    </div>
+                    <div>
+                        <p><img src="../../../Vendor/images/heineken.png"></p>
+                        <p>Name 1: some info</p>
+                    </div>
+                </div> 
+
+                <div class="col-lg-12 last_sold">
+                    <h2 class="welcome">LAST 3 SOLD BEERS</h2>
+                    <div>
+                        <p><img src="../../../Vendor/images/heineken.png" ></p>
+                        <p>Name 1: some info</p>
+                    </div>
+                    <div>
+                        <p><img src="../../../Vendor/images/heineken.png"></p>
+                        <p>Name 1: some info</p>
+                    </div>
+                    <div>
+                        <p><img src="../../../Vendor/images/heineken.png"></p>
+                        <p>Name 1: some info</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
     
-    <div class="main_info">
-        <h2 class="welcome">Welcome to Quality House Beer Website</h2>
-        <div class="main_text">
-            <p>We are Quality House Beer Company and we started our beer production business in 2015. </p>
-            <p>Our mission is to provide the customer with the desired pleasure of consuming quality beer. </p>
-            <p>That's why we started this project with enthusiasm and over the years we have made efforts <br> to improve the taste of our beer.</p>
-        </div>
-    </div>
-
-    <div class="beers">
-        <div class="best_beers">
-            <h2 class="welcome"> BEST SELLING BEERS</h2>
-
-            <div>
-                <p><img src="../../../Vendor/images/heineken.jpg" style="width:100px; height:100px"></p>
-                <p>Name 1</p>
-                <p>some info</p>
-            </div>
-            <div>
-                <p><img src="../../../Vendor/images/heineken.jpg" style="width:100px; height:100px"></p>
-                <p>Name 1</p>
-                <p>some info</p>
-            </div>
-            <div>
-                <p><img src="../../../Vendor/images/heineken.jpg" style="width:100px; height:100px"></p>
-                <p>Name 1</p>
-                <p>some info</p>
-            </div>
-            <div>
-                <p><img src="../../../Vendor/images/heineken.jpg" style="width:100px; height:100px"></p>
-                <p>Name 1</p>
-                <p>some info</p>
-            </div>
-            <div>
-                <p><img src="../../../Vendor/images/heineken.jpg" style="width:100px; height:100px"></p>
-                <p>Name 1</p>
-                <p>some info</p>
-            </div>
-        </div>
-
-
-
-        <div class="best_beers">
-            <h2 class="welcome"> LAST 3 SOLD BEERS</h2>
-            <div>
-                <p><img src="../../../Vendor/images/heineken.jpg" style="width:100px; height:100px"></p>
-                <p>Name 1</p>
-                <p>some info</p>
-            </div>
-            <div>
-                <p><img src="../../../Vendor/images/heineken.jpg" style="width:100px; height:100px"></p>
-                <p>Name 1</p>
-                <p>some info</p>
-            </div>
-            <div>
-                <p><img src="../../../Vendor/images/heineken.jpg" style="width:100px; height:100px"></p>
-                <p>Name 1</p>
-                <p>some info</p>
-            </div>
-        </div>
-    </div>
+    
+    
 
     <footer>
       <?php include_once "../beer/footer.php"; ?>
     </footer> 
     
+
     </body>
 </html>
