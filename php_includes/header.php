43cffe76bed6499cf27bb1d35ed5e9420953585a
TodorMiroleskov <tmiroleskov@qualityhouse.com>
2018-03-21 11:10:34 +0200

    Fix issue with half working logo

--- php_includes/header.php
+++ php_includes/header.php
@@ -1,14 +1,14 @@
-        <h1>Quality House Beer</h1>
+        <h1 style="width:50%; margin:0 auto;">Quality House Beer</h1>
         <nav>
-        <a href="index.php" class="navbar-left"><img src="images/logoNew_bubbles.png"></a>
+        <a href="index.php"><img src="images/logoNew_bubbles.png"></a>
             <ul>
                 <li><a id="home" href="index.php"><i class="fa fa-home"></i> HOME</a></li>
                 <li><a id="catalog" href="catalog.php"><i class="fa fa-list"></i> CATALOG</a></li>
                 <li><a id="about" href="about.php"><i class="fa fa-pencil"></i> ABOUT</a></li>
                 <li><a id="register" href="register.php"><i class="fa fa-user"></i> REGISTER</a></li>
-                <li><a id="login" href="login.php"><i class="fa fa-user"></i>LOGIN</a></li>
+                <li><a id="login" href="login.php"><i class="fa fa-user"></i> LOGIN</a></li>
                 <li><a id="basket" href="basket.php"><i class="fa fa-beer"></i> BASKET</a></li>
                 <li><a id="faq" href="faq.php"><i class="fa fa-question"></i> FAQ</a></li>
-				<li><a href="#"><i class="fa fa-search"></i><input style="box-sizing: border-box; border: 2px solid; border-radius: 15px; height: 40px;" type="text" placeholder=" Search..." name="search"></a></li>
+				<li><a href="#"><i class="fa fa-search"></i> <input style="box-sizing: border-box; border: 2px solid; border-radius: 15px; height: 40px;" type="text" placeholder=" Search..." name="search"></a></li>
             </ul>
         </nav>
