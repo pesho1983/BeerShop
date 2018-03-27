131e6b024df19a8a936facd6e171cdf6ce91714f
Petar Aleksandrov <paleksandrov@qualityhouse.com>
2018-03-27 17:39:15 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,10 +2,10 @@
 ini_set('session.cookie_lifetime', 60 * 60 * 24 * 30);
 session_start();
 //Our MySQL user account.
-define('MYSQL_USER', 'qh-beer');
+define('MYSQL_USER', 'root');
 
 //Our MySQL password.
-define('MYSQL_PASSWORD', 'pass');
+define('MYSQL_PASSWORD', '');
 
 //The server that MySQL is located on.
 define('MYSQL_HOST', 'localhost');
