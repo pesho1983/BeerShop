17a77b2c0b2d523ec19426fccea7001546e656e0
eXpLoD <mihael_a@abv.bg>
2018-04-18 17:54:56 +0300

    fixes

--- cartAction.php
+++ cartAction.php
@@ -1,9 +1,10 @@
 <?php
 require_once 'Cart.php';
+require_once 'connect.php';
 $cart = new Cart;
 
 // include database configuration file
-require_once 'connect.php';
+
 if(isset($_REQUEST['action']) && !empty($_REQUEST['action'])){
     if($_REQUEST['action'] == 'addToCart' && !empty($_REQUEST['id'])){
         $productID = $_REQUEST['id'];
